0eb8d2210b2b788e5cebcf0d2c2ceba872300e3e
Santiago Jaramillo M <78182976+sjara11@users.noreply.github.com>
2023-04-04 11:36:17 -0500

    Update streamlit_app.py
    handle empty input in fruityvice api

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit
 import pandas
 import requests
 import snowflake.connector
+from urllib.error import URLError
 
 streamlit.title('Breakfast Favorites')
 
@@ -17,15 +18,17 @@ streamlit.dataframe(fruits_to_show)
 
 # Fruityvice API
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice )
-
-# Prettier json output
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# Print as table
-streamlit.dataframe(fruityvice_normalized)
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    streamlit.error("please select a fruit")
+   else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice )
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+  streamlit.error()
 
 #snowflake connection
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
