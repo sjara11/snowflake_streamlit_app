0273ef45b88a20925f2b905271d1bcb125f79621
Santiago Jaramillo M <78182976+sjara11@users.noreply.github.com>
2023-04-04 09:17:22 -0500

    Update streamlit_app.py
    Create a table with the json output

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,4 +18,8 @@ streamlit.dataframe(fruits_to_show)
 # Fruityvice API
 streamlit.header("Fruityvice Fruit Advice!")
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+
+# Prettier json output
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# Print as table
+streamlit.dataframe(fruityvice_normalized)
